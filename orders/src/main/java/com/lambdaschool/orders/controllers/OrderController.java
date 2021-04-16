730de6359263b2025c33255adf95d168b87f2c1e
Taylor Friesen <taylorfriesen@protonmail.com>
2021-04-15 19:50:50 -0700

    POST /orders/order working

--- orders/src/main/java/com/lambdaschool/orders/controllers/OrderController.java
+++ orders/src/main/java/com/lambdaschool/orders/controllers/OrderController.java
@@ -3,12 +3,13 @@ package com.lambdaschool.orders.controllers;
 import com.lambdaschool.orders.models.Order;
 import com.lambdaschool.orders.services.OrderServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/orders")
@@ -21,5 +22,17 @@ public class OrderController {
         System.out.println("test");
         Order order = orderServices.findOrderByNum(num);
         return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/order", consumes = "application/json")
+    public ResponseEntity<Void> addOrder(@RequestBody Order order) {
+        order = orderServices.save(order);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newCustomerURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{ordnum}")
+                .buildAndExpand(order.getOrdnum())
+                .toUri();
+        responseHeaders.setLocation(newCustomerURI);
+        return new ResponseEntity<>(responseHeaders, HttpStatus.CREATED);
     }
 }
--- orders/src/main/java/com/lambdaschool/orders/services/OrderServices.java
+++ orders/src/main/java/com/lambdaschool/orders/services/OrderServices.java
@@ -4,4 +4,6 @@ import com.lambdaschool.orders.models.Order;
 
 public interface OrderServices {
     Order findOrderByNum(long num);
+
+    Order save(Order order);
 }
